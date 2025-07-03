db92419b358f376396db829972d020afbeb85918
niyokuru2 <niyonkurusam5@gmail.com>
2025-07-03 11:04:15 +0200

    initial commit

---
+++ app/api/clerk/route.js
@@ -0,0 +1,45 @@
+import { Webhook } from "svix";
+import connectDb from "@/config/db";
+import User from "@/models/User";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+export async function POST() {
+    const wh = new Webhook(process.env.SIGNING_SECRET)
+    const headerPayload = await headers()
+    const svxHeaders ={
+        "svix-id":headerPayload.get("svix-id"),
+        "svix-signature":headerPayload.get("svix-signature"),
+    }
+    //get the payload and verify it
+
+    const payload = await req.json();
+    const body = JSON.stringify(payload);
+    const {data,type} = wh.verify(body,svxHeaders)
+
+    //Prepare the user data to be saved in the database
+
+    const userData={
+        _id:data.id,
+        email:data.email_addresses[0].email_addresses,
+        name:`${data.first_name} ${data.last_name}`,
+        image:data.image_url,
+    }
+    await connectDb();
+
+    switch(type){
+        case 'user.created':
+            await User.create(userData)
+            break;
+    case'user.updated':
+            await User.findByIdAndUpdate(data.id,userData)
+            break;
+    case'user.deleted':
+            await User.findByIdAndDelete(data.id)
+            break;
+
+    default:
+        break;
+
+    }
+    return NextResponse.json({message:"Event Received"})
+}
---
+++ assets/PromptBox.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PromptBox = () => {
+  return (
+    <div className={`w-full ${false ? "max-w-3xl" : "max-w-2xl"}`}>PromptBox</div>
+  )
+}
+
+export default PromptBox
